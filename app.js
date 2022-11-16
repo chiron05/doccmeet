b9a86c29713370964a643dee43ab6b404f162a19
sarthak <sarthaknaik010@gmail.com>
2022-11-16 10:12:47 +0530

    localhost 3001

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose=require('mongoose');
 const { User } = require('./Model/userModel');
 const router = require('./Routes/index');
 const app=express();
-const PORT=process.env.PORT||3000
+const PORT=process.env.PORT||3001
 const cors=require('cors')
 
 app.use(express.json())
