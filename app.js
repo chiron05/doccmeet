56aa014021b284eaf150b391569f922edeb9ea08
sarthak <sarthaknaik010@gmail.com>
2022-11-16 10:19:00 +0530

    cors option

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const { User } = require('./Model/userModel');
 const router = require('./Routes/index');
 const app=express();
 const PORT=process.env.PORT||3001
-const cors=require('cors')
+const cors = require('cors');
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use(router)
 
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+
 
 mongoose.connect(process.env.MONGODB_URL_ATLAS,{
     useNewUrlParser:true,
